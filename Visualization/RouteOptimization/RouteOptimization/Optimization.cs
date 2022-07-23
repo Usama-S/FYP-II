51782172f5192f25257a13808c701cd236ce4f96
Usama Sultan <usamasultan.pk@gmail.com>
2022-07-23 14:00:15 +0500

    Added algorithm flowchart

---
+++ Visualization/RouteOptimization/RouteOptimization/Optimization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RouteOptimization
+{
+    public class Optimization
+    {
+        public static void Optimize()
+        {
+            // your code here...
+        }
+
+        public static void Filtering()
+        {
+            // yoru code here...
+        }
+
+        public static void GetShortestRoute()
+        {
+            // your code here...
+        }
+
+        public static void GetFitnessValue()
+        {
+            // your code here...
+        }
+    }
+}
